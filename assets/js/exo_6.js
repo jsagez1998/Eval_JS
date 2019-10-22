90f4b8d76f890d55e10255132baf1594ecdd9a67
Jason <51094681+jsagez1998@users.noreply.github.com>
2019-10-22 16:18:29 +0200

    Exo_6 vFinalCheck
    Resolution d'un bug de condition

--- assets/js/exo_6.js
+++ assets/js/exo_6.js
@@ -1,7 +1,7 @@
 var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
 var name = prompt("Quel nom rechercher ?"); 
 var pos = tab.indexOf(name);
-if(pos = -1){
+if(pos < 0){
     alert("Le nom n'est pas présent dans le tableau");
 }else{
     tab.splice(pos, 1);
