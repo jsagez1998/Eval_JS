19f5de663dc28e2c3dbd662a21b54b4ebea7b9ea
Jason <51094681+jsagez1998@users.noreply.github.com>
2019-10-22 16:17:08 +0200

    Eval_JS v6
    Integration de l'exercice 6

--- assets/js/eval.js
+++ assets/js/eval.js
@@ -137,3 +137,20 @@ function TableMultiplication(number){
         console.log(i+" x "+number+" = "+res);
         }
 }
+//------------------------------------------------------------------------------------------------------------------
+//Exercice 6
+
+var btnExo6 = document.getElementById("btnExo6");
+
+btnExo6.addEventListener("click", function(){
+    var tab = ["Audrey", "Aurélien", "Flavien", "Jérémy", "Laurent", "Melik", "Nouara", "Salem", "Samuel", "Stéphane"];
+    var name = prompt("Quel nom rechercher ?"); 
+    var pos = tab.indexOf(name);
+    if(pos < 0){
+        alert("Le nom n'est pas présent dans le tableau");
+    }else{
+        tab.splice(pos, 1);
+        tab.push(""); 
+        console.log(tab); 
+}
+});
