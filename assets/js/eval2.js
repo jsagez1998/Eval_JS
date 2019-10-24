654a3e76d1bb3f3bf63160bd3aa3bc75bd8e35cb
Jason <51094681+jsagez1998@users.noreply.github.com>
2019-10-24 15:55:29 +0200

    Eval_JS v7.3
    Séparation des déclarations de variables des fonctions et des events ps: name n'affiche pas de messages d'erreur

--- assets/js/eval2.js
+++ assets/js/eval2.js
@@ -1,24 +1,98 @@
 // Exercice 7
-//Prend en compte tous les caractères alphabétiques
-var oneCaract = RegExp("[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]+");
-//Si code postal entrez mettre 5 chiffres
-var numeric5 = RegExp("^[0-9]{5}$");
-// 1 2 ou 3 chiffres pour adresse
-var regexAdress = RegExp("^[0-9]{1,3}(?:\s[a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ]){2,}$");
- 
-var btnForm = document.getElementById("send");
-var name = document.getElementById("nom");
-var missName = document.getElementById("missName");
-var fname = document.getElementById("fname")
+function styleError(elem) {
+  //factorisation du code modifiant le style
+  elem.style.color = "red";
+  elem.style.display = "block";
+}
 
-btnForm.addEventListener("click", function(event){
-    //Si le champ est vide
-    if(name.validity.valueMissing){
+function styleValid(elem) {
+  //factorisation du code modifiant le style
+  elem.style.color = "black";
+  elem.style.display = "inline";
+}
+
+function checkForm(event) {
+    // Fonction appelée par addEventlistener
+    if (!Caract.test(name.value)) {
+        // Nom valide ?
         event.preventDefault();
-        missName.textContent = "Nom manquant";
-    //Si le format de données est incorrect
-    }else if(!oneCaract.test(name.value)){
+        missName.textContent ="Entrez un nom valide";
+        styleError(missName);
+      }else{
+        missName.textContent = "*";
+        styleValid(missName);
+      }
+
+      if (!Caract.test(fname.value)) {
+        // Prénom valide ?
         event.preventDefault();
-        missName = "Format incorrect";
-    }
-});
+        missFname.textContent =
+          "Entrez un prénom valide";
+        styleError(missFname);
+      }else{
+        missFname.textContent = "*";
+        styleValid(missFname);
+      }
+    
+      if (date.value == "") {
+        event.preventDefault();
+        missDate.textContent = "Date de naissance invalide";
+        styleError(missDate);
+      }else if(!rxDate.test(date.value)) {
+        // IE ne prend pas en charge les input date donc on teste si la date respect la Regex
+        event.preventDefault();
+        missDate.textContent = "Date de naissance invalide";
+        styleError(missDate);
+      }else{
+        missDate.textContent = "*";
+        styleValid(missDate);
+      }
+
+      if(!(adress.value ==""|| rxAdress.test(adress.value))) {
+        event.preventDefault();
+        missAdr.textContent ="Adresse invalide : Entrez une adresse valide ou rien";
+        styleError(missAdr);
+      }else{
+        missAdr.textContent = "*";
+        styleValid(missAdr);
+      }
+
+      if(!(homme.checked || femme.checked)) {
+        event.preventDefault();
+        missGenre.textContent = "Veuillez sélectionner votre sexe";
+        styleError(missGenre);
+      }else{
+        missGenre.textContent = "*";
+        styleValid(missGenre);
+      }
+
+      if(!rxMail.test(email.value)){
+        event.preventDefault();
+        missMail.textContent = "Adresse mail invalide";
+        styleError(missMail);
+      }else{
+        missMail.textContent = "*";
+        styleValid(missMail);
+      }
+
+      if(Suj.selectedIndex==-1) {
+        event.preventDefault();
+        missSuj.textContent = "Sélectionnez un sujet";
+        styleError(missSuj);
+      }else{
+        missSuj.textContent = "*";
+        styleValid(missSuj);
+      }
+
+      if(Question.value==""){
+        event.preventDefault();
+        missQuest.textContent = "Veuillez entrer votre question";
+        styleError(missQuest);
+      }else{
+        missQuest = "*";
+        styleValid(missQuest);
+      }
+  }
+
+  btnForm.addEventListener("click", checkForm);
+  
